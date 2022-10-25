bed50071b3d1b8a75d7461366387f7f06a9981ee
HongKilDong <ekdma3259@gmail.com>
2022-10-25 15:31:06 +0900

    PC commit 4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from add import add_func
-from mul import mul_func
+from mul import sub_func
 ## 함수 선언부
 
 ## 전역 변수부
@@ -9,5 +9,5 @@ num1, num2, res = 100, 200, 0
 res = add_func(num1, num2)
 print(num1, "+", num2, "=", res)
 
-res = mul_func(num1, num2)
-print(num1, "*", num2, "=", res)
+res = sub_func(num1, num2)
+print(num1, "-", num2, "=", res)
